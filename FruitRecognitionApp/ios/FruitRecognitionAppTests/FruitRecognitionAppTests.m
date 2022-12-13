45a81e828b8e16f66d1a6529fb8a7505cca8a11c
Ondrej Hruby <o.hruby@student.fontys.nl>
2022-12-13 14:53:55 +0100

    Fixed some tweaks

--- FruitRecognitionApp/ios/FruitRecognitionAppTests/FruitRecognitionAppTests.m
+++ FruitRecognitionApp/ios/FruitRecognitionAppTests/FruitRecognitionAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface CalistFitnessAppTests : XCTestCase
+@interface FruitRecognitionAppTests : XCTestCase
 
 @end
 
-@implementation CalistFitnessAppTests
+@implementation FruitRecognitionAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
